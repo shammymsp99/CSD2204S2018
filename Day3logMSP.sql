6275c961c8c0ae013adcedb27a919956d1b9d859
Shashank Preetham Machani <39058548+shammymsp99@users.noreply.github.com>
2018-05-09 11:31:33 -0400

    Add files via upload

---
+++ Day3logMSP.sql
@@ -0,0 +1,17 @@
+MariaDB [(none)]> show variables like 'version';
++---------------+-----------------+
+| Variable_name | Value           |
++---------------+-----------------+
+| version       | 10.1.21-MariaDB |
++---------------+-----------------+
+1 row in set (0.00 sec)
+
+MariaDB [(none)]> show databases;
++--------------------+
+| Database           |
++--------------------+
+| information_schema |
+| test               |
++--------------------+
+2 rows in set (0.00 sec)
+
